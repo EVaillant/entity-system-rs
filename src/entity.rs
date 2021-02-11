a372b469682002f21a54c6078d024d9928a73dae
Etienne Vaillant <vaillant.etienne@gmail.com>
2021-02-11 11:47:01 +0100

    Add API documentation on Entity and Storage

--- src/entity.rs
+++ src/entity.rs
@@ -1,22 +1,79 @@
 use std::collections::HashSet;
 
+///
+/// `Entity` type, as seen by the user.
 #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
 pub struct Entity {
     pub id: u32,
 }
 
 impl Entity {
+    ///
+    /// Create a new `Entity`
     pub fn new(id: u32) -> Self {
         Self { id }
     }
 }
 
+///
+/// Entity Factory
+/// 
+/// # Example
+/// 
+/// * Allocation & free Entity
+/// ```rust
+/// use entity_system::EntityAllocator;
+/// 
+/// // create allocator
+/// let mut allocator = EntityAllocator::new();
+/// 
+/// // alloc a new entity
+/// let entity1 = allocator.alloc();
+/// let id1 = entity1.id;
+/// 
+/// // free the entity
+/// allocator.free(entity1);
+/// 
+/// // re-alloc an entity
+/// let entity2 = allocator.alloc();
+/// let id2 = entity2.id;
+/// assert!(id2 == id1);
+/// ```
+/// 
+/// * Iter
+/// ```rust
+/// use entity_system::{EntityAllocator, Entity};
+/// 
+/// let mut allocator = EntityAllocator::new();
+/// 
+/// // alloc 5 entity
+/// for _ in 0..5 {
+///     allocator.alloc();
+/// }
+/// 
+/// // free 2 entities
+/// allocator.free(Entity::new(1));
+/// allocator.free(Entity::new(3));
+/// 
+/// // iter over allocated entity
+/// for entity in allocator.iter() {
+///     println!("{}", entity.id);
+/// }
+/// ```
+/// Output will be :
+/// ```text
+/// 0
+/// 2
+/// 4
+/// ```
 pub struct EntityAllocator {
     next: Entity,
     free: HashSet<Entity>,
 }
 
 impl EntityAllocator {
+    ///
+    /// Create a new `EntityAllocator`
     pub fn new() -> Self {
         Self {
             next: Entity::new(0),
@@ -24,6 +81,8 @@ impl EntityAllocator {
         }
     }
 
+    ///
+    /// Alloc a new `Entity`
     pub fn alloc(&mut self) -> Entity {
         match self.free.iter().next() {
             Some(&value) => {
@@ -38,10 +97,14 @@ impl EntityAllocator {
         }
     }
 
+    ///
+    /// Free an `Entity`. `Entity` id could be re-used
     pub fn free(&mut self, entity: Entity) {
         self.free.insert(entity);
     }
 
+    ///
+    /// Iter over allocated `Entity`
     pub fn iter(&self) -> EntityAllocatorIterator {
         EntityAllocatorIterator::new(self)
     }
@@ -53,12 +116,17 @@ impl Default for EntityAllocator {
     }
 }
 
+///
+/// EntityAllocatorIterator over EntityAllocator
+/// cf [`EntityAllocator`] to have an example
 pub struct EntityAllocatorIterator<'a> {
     allocator: &'a EntityAllocator,
     current: Entity,
 }
 
 impl<'a> EntityAllocatorIterator<'a> {
+    ///
+    /// Create an Iterator
     pub fn new(allocator: &'a EntityAllocator) -> Self {
         let mut it = Self {
             allocator,
--- src/storage.rs
+++ src/storage.rs
@@ -1,13 +1,65 @@
 use crate::entity::Entity;
 
+///
+/// Trait must be implemented to store [`crate::Composant`]
 pub trait Storage<T> {
+    ///
+    /// Allocation an item in the storage
+    /// 
+    /// # Panics
+    /// 
+    /// If allocation failed
     fn alloc(&mut self, entity: Entity);
+
+    ///
+    /// Free the item in the storage
     fn free(&mut self, entity: Entity);
+
+    ///
+    /// Get item from storage
+    /// 
+    /// # Panics
+    /// 
+    /// If no allocation has be done before
     fn get(&self, entity: Entity) -> &T;
+
+    ///
+    /// Get item from storage (mutable version)
+    /// 
+    /// # Panics
+    /// 
+    /// If no allocation has be done before
     fn get_mut(&mut self, entity: Entity) -> &mut T;
+
+    ///
+    /// Check if allocatio has been done    
     fn has(&self, entity: Entity) -> bool;
 }
 
+///
+/// Implementation of Storage<T> with a [`Vec`] as underlying.
+/// 
+/// # Example
+/// ```rust
+///     use entity_system::{Entity, Storage, BasicVecStorage};
+/// 
+///     let mut storage : BasicVecStorage<u32> = Default::default();
+///     let entity = Entity::new(0);
+/// 
+///     // allocation (default value is 0)
+///     storage.alloc(entity);
+/// 
+///     // read the value
+///     let val = storage.get(entity);
+///     assert!(*val == 0);
+/// 
+///     // update the value
+///     let val = storage.get_mut(entity);
+///     *val = 5;
+/// 
+///     // free
+///     storage.free(entity); 
+/// ```
 #[derive(Default)]
 pub struct BasicVecStorage<T>
 where
